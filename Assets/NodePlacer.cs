02008c4b88226de429ece8d3fd950c6182c9c095
Alessandro Buonpane <ale.buonpane@gmail.com>
2017-09-25 17:54:40 +0200

    node links correct

---
+++ Assets/NodePlacer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodePlacer : MonoBehaviour {
+
+    [SerializeField]
+    GameObject node;
+
+    [SerializeField]
+    Vector2 bottomLeftCorner;
+
+    [SerializeField]
+    Vector2 topRightCorner;
+
+    [SerializeField]
+    float precision; 
+
+    float stepX;
+    float stepY;
+
+    List<Node> nodes = new List<Node>(); 
+
+    private void Start()
+    {
+        PlaceNodes();
+        ConnectNodes(); 
+    }
+
+    void PlaceNodes()
+    {
+        stepX = (topRightCorner.x - bottomLeftCorner.x)/ precision;
+        stepY = (topRightCorner.y - bottomLeftCorner.y)/ precision;
+        float x = bottomLeftCorner.x;
+        float y = bottomLeftCorner.y;
+
+        GameObject folder = new GameObject();
+        folder.name = "Nodes"; 
+
+        for(int i = 0; i<precision; i++)
+        {
+            for (int j = 0; j < precision; j++)
+            {
+                x += stepX;
+                GameObject n = Instantiate(node, new Vector3(x, y, 0), Quaternion.identity);
+                n.transform.SetParent(folder.transform);
+                nodes.Add(n.GetComponent<Node>()); 
+            }
+            y += stepY;
+            x = bottomLeftCorner.x; 
+        }
+    
+    }
+
+    void ConnectNodes()
+    {
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            nodes[i].FindValidNeighbours(stepX,stepY); 
+        }
+    }
+}
